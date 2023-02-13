b83b30bd4f2be867f2e7a6acfc81a4451ee01ce3
sashagarsson <sasha.garsson@gmail.com>
2023-02-13 15:02:00 -0600

    finishes

--- main.js
+++ main.js
@@ -16,11 +16,31 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+const numToString =() => {
+  let num = document.getElementById("num-to-str").value;
+  // let num = 15;
+let text = num.toString();
+
+
+document.getElementById("number").innerHTML = text;
+}
+
 
 
 // Write a JavaScript program to convert a string to the number.
 
+const strToNumber =() => {
 
+  let str=document.getElementById("str-to-num").value;
+
+  let num = Number(str);
+
+  console.log(`This is a ${typeof num}`)
+
+  document.getElementById("results").innerHTML = num;
+
+
+}
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -29,22 +49,94 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
+
+  const findTypeOf  =() => {
+  
+let a = true; 
+
+
+let b = null;
   
 
-  
-// Write a JavaScript program that adds 2 numbers together.
+let c;
+
+let d=32
+
+let e = isNaN('Hello')
+if (e == true) {
+  e ="e is a NaN"
+  // console.log(`e is a NaN`)
+}
+
+let f = "Hello"
+
+let h2 = document.getElementById("practice")
+let html =
+`<p>a is a ${typeof a} </p>
+<p>b is an object set to null </p>
+<p>c is a ${typeof c} </p>
+<p>d is a ${typeof d} </p>
+<p> ${e} </p>
+<p>f is a ${typeof f} </p>`;
+
+h2.insertAdjacentHTML("afterend", html);
+
+  };
 
 
+
+const addTwoNums = () => {
+
+  let numOne = document.getElementById("num1").valueAsNumber
+  let numTwo = document.getElementById("num2").valueAsNumber
+
+  sum = numOne + numTwo
+  document.getElementById("sum-of-numbers").innerHTML=sum
+}
 
 // Write a JavaScript program that runs only when 2 things are true.
 
 
+const isEven = (num) => {
+  if(num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
+console.log(isEven(2))
+console.log(isEven(4))
+
+}
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
 
+const isOdd = (num) => {
+  if(num % 2 !== 0) {
+    return true
+  } else {
+    return false
+  }
+  console.log(2)
+  console.log(5)
+
+}
+
 
 // Write a JavaScript program that runs when both things are not true.  
+
+
+const ifEven = (num) => {
+  if(num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
+  console.log(3)
+  console.log(5)
+
+
+}
 
 // ***************************
 //         PART TWO
